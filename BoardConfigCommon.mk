f8cfa78a8cb705d84c9bd53ab2f33f1518363fb1
thekraven <savethestyle1@gmail.com>
2014-01-29 04:06:47 -0600

    more bt fix from armv6
    Change-Id: Ia8c8ae2a8cb4fe1dcf3729558fbc2473f9cd4b75

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,18 +20,9 @@ include device/qcom/msm7x27/BoardConfigCommon.mk
 
 ## Bluetooth
 BOARD_HAVE_BLUETOOTH_BCM := true
-<<<<<<< HEAD
-=======
 BOARD_HAVE_BLUETOOTH := true
 BOARD_BLUEDROID_VENDOR_CONF := device/lge/msm7x27-common/bluetooth/libbt_lge.txt
->>>>>>> c979cbb... Remove bluez support (1/2)
 
-# Note: BOARD_HAVE_BLUETOOTH_BLUEZ is defined in 
-# device/lge/msm7x27-common/device.mk due to inheritance issues. 
-ifndef BOARD_HAVE_BLUETOOTH_BLUEZ 
-  BOARD_HAVE_BLUETOOTH := true 
-  BOARD_BLUEDROID_VENDOR_CONF := device/lge/msm7x27-common/bluetooth/libbt_lge.txt 
-endif
 ## Boot loader & recovery
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
