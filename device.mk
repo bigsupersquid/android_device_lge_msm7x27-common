0ee419ce11870fa66b075c89ac27259bd3d01174
AndroidMeda <hephappy@gmail.com>
2014-02-27 06:48:36 +0100

    Revert "Turn on BLE, GATT, and SMP of Bluetooth stack"
    This reverts commit e18fbd391c4cacf7ca90e494874d524b3d771482.
    
    Change-Id: Ia7e7190cf89bb29cda747e8f8134e596c3090fa9

--- device.mk
+++ device.mk
@@ -26,7 +26,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/default.prop:root/default.prop
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
